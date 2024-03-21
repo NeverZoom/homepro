fc4a338f08c5104e1b6f4250be693c12d8ad80bb
Кирилл Панченко <mrneverzoom@gmail.com>
2024-03-21 16:16:59 +0300

    fixed

--- app/js/app.js
+++ app/js/app.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
 				start: "center center", // when the top of the trigger hits the top of the viewport
 				end: "+="+innerHeight, // end after scrolling 500px beyond the start
 				scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-				markers: true,
+				// markers: true,
 			}
 		});
 		// add animations and labels to the timeline
